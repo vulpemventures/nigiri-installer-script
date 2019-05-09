8c62daf24d72037ea965d4256ba77da725cbec63
Marco Argentieri <marco@vulpem.com>
2019-05-10 01:38:15 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -4,3 +4,7 @@ aws s3 cp ./index.sh s3://getnigiri.vulpem.com/index.sh \
   --content-type 'text/plain' \
   --cache-control 'max-age=60' \
   --acl public-read
+
+aws cloudfront create-invalidation \
+  --distribution-id ERBVZ5EZBWH8Z \
+  --paths '/*'
