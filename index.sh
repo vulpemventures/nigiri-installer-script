309d39817562e8f22673ee9c7a5e6b8fb5afb8d7
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2020-08-04 10:50:40 +0200

    Do not exit if Docker is not running

--- index.sh
+++ index.sh
@@ -15,10 +15,9 @@ function remove_images() {
     $repo/electrs-liquid:latest
     $repo/esplora-liquid:latest
   )
-  for image in ${images[*]}
-  do
+  for image in ${images[*]}; do
     if [ "$(docker images -q $image)" != "" ]; then
-      docker rmi $image 1> /dev/null
+      docker rmi $image 1>/dev/null
       echo "successfully deleted $image"
     fi
   done
@@ -28,16 +27,22 @@ function remove_images() {
 ##|      DETECT PLATFORM      |
 ##\=====================================/
 case $OSTYPE in
-  darwin*) OS="darwin";;
-  linux-gnu*) OS="linux";;
-  *) echo "OS $OSTYPE not supported by the installation script"; exit 1;;
+darwin*) OS="darwin" ;;
+linux-gnu*) OS="linux" ;;
+*)
+  echo "OS $OSTYPE not supported by the installation script"
+  exit 1
+  ;;
 esac
 
 case $(uname -m) in
-  armv7l) ARCH="arm";;
-  amd64) ARCH="amd64";;
-  x86_64) ARCH="amd64";;
-  *) echo "Architecture $ARCH not supported by the installation script"; exit 1;;
+armv7l) ARCH="arm" ;;
+amd64) ARCH="amd64" ;;
+x86_64) ARCH="amd64" ;;
+*)
+  echo "Architecture $ARCH not supported by the installation script"
+  exit 1
+  ;;
 esac
 
 BIN="$HOME/bin"
@@ -52,10 +57,12 @@ if [ "$(command -v nigiri)" != "" ]; then
   if [ -z "$(docker info 2>&1 >/dev/null)" ]; then
     :
   else
-    echo "Error: when uninstalling an old Nigiri version Docker must be running."
     echo
-    echo "Please, start the Docker daemon before launching this installation script."
-    exit 1
+    echo "Info: when uninstalling an old Nigiri version Docker must be running."
+    echo
+    echo "Be sure to start the Docker daemon before launching this installation script."
+    echo
+    #exit 1
   fi
 
   echo "Stopping Nigiri..."
@@ -89,27 +96,27 @@ RELEASE_URL="$NIGIRI_URL/download/$TAG/nigiri-$OS-$ARCH"
 
 echo "Fetching $RELEASE_URL..."
 
-curl -sL $RELEASE_URL > nigiri
+curl -sL $RELEASE_URL >nigiri
 
 case $SHELL in
-  *zsh) PROFILE="$HOME/.zshrc";;
-  *ksh) PROFILE="$HOME/.kshrc";;
-  *bash)
-    if [ -f "$HOME/.bash_profile" ]; then
-      PROFILE="$HOME/.bash_profile"
-    elif [ -f "$HOME/.bash_login" ]; then
-      PROFILE="$HOME/.bash_login"
-    elif [ -f "$HOME/.profile" ]; then
-      PROFILE="$HOME/.profile"
-    fi
-    ;;
-  *csh)
-    if [ -f "$HOME/.tcshrc" ]; then
-      PROFILE="$HOME/.tcshrc"
-    elif [ -f "$HOME/.cshrc" ]; then
-      PROFILE="$HOME/.cshrc"
-    fi
-    ;;
+*zsh) PROFILE="$HOME/.zshrc" ;;
+*ksh) PROFILE="$HOME/.kshrc" ;;
+*bash)
+  if [ -f "$HOME/.bash_profile" ]; then
+    PROFILE="$HOME/.bash_profile"
+  elif [ -f "$HOME/.bash_login" ]; then
+    PROFILE="$HOME/.bash_login"
+  elif [ -f "$HOME/.profile" ]; then
+    PROFILE="$HOME/.profile"
+  fi
+  ;;
+*csh)
+  if [ -f "$HOME/.tcshrc" ]; then
+    PROFILE="$HOME/.tcshrc"
+  elif [ -f "$HOME/.cshrc" ]; then
+    PROFILE="$HOME/.cshrc"
+  fi
+  ;;
 esac
 
 echo "Moving binary to $BIN..."
@@ -120,7 +127,7 @@ chmod +x $BIN/nigiri
 
 if [ "$PATH" != *"$BIN"* ]; then
   echo "Exporting path..."
-  echo "export PATH=\$PATH:\$HOME/bin" >> $PROFILE
+  echo "export PATH=\$PATH:\$HOME/bin" >>$PROFILE
 fi
 
 echo "Creating data directory ~/.nigiri..."
@@ -145,12 +152,12 @@ curl -o $LIQUID_FOLDER/liquid-config/elements.conf -sL "$URL/$LIQUID_VOLUME/liqu
 
 echo "Checking for Docker and Docker compose..."
 if [ "$(command -v docker)" == "" ]; then
-  echo "Warning: Nigiri uses Docker and it seems not to be installed, check the official documentation for downloading it.";
+  echo "Warning: Nigiri uses Docker and it seems not to be installed, check the official documentation for downloading it."
   if [ "$OS" = "darwin" ]; then
     echo "https://docs.docker.com/v17.12/docker-for-mac/install/#download-docker-for-mac"
   else
     echo "https://docs.docker.com/v17.12/install/linux/docker-ce/ubuntu/"
-  fi 
+  fi
 fi
 
 echo ""
